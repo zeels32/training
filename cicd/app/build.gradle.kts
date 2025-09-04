ce37750c8e899eb0cf6e3d1d906316be8cd7853d
Zeel <zeel.chetan@anywhere.co>
2025-09-04 11:47:47 +0530

    Generate Release build with jks file
    (cherry picked from commit cf394018bc384c4988886060776974799590dcfd)

--- cicd/app/build.gradle.kts
+++ cicd/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.release
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -17,12 +19,25 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("CD_KEYSTORE_FILE") ?: "cicd/cdapp.jks")
+            storePassword = System.getenv("CD_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("CD_KEYSTORE_ALIAS")
+            keyPassword = System.getenv("CD_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
-        release {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
